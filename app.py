84138e26f52edec4c663c1eeb1eff5341c3faf0b
SUZUKI Keiko <88998611+Diff2723@users.noreply.github.com>
2023-05-29 13:18:46 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
-        speaker = "🙂"
+        speaker = "٩(ˊᗜˋ*)و🙂"
         if message["role"]=="assistant":
-            speaker="🤖"
+            speaker="( '-' 🐱 )ﾈｺﾁｬﾝ."
 
         st.write(speaker + ": " + message["content"])
