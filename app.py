5368bb847bdd53da20d25ee5476acdcd2cbba49f
SUZUKI Keiko <88998611+Diff2723@users.noreply.github.com>
2023-05-29 13:15:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("V-pets")
+st.write("ChatGPT APIを使った猫ボットです。")
 
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+user_input = st.text_input("ペットの猫に話しかけてね。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
